2bc4a7f1aafb5131eb86df4b0851336f0fcf805a
Zackweb56 <zackweb56@gmail.com>
2025-11-01 20:11:18 +0100

    test: see if its works now root page in vercel

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,4 @@
 // src/app/page.js
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { AiOutlineLoading3Quarters } from 'react-icons/ai';
-
-export default function RootPage() {
-  const router = useRouter();
-  
-  useEffect(() => {
-    router.replace('/home');
-  }, [router]);
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <AiOutlineLoading3Quarters className="text-4xl text-blue-600 animate-spin" />
-    </div>
-  );
+export default function Home() {
+  return <h1>TEST - If you see this, it works!</h1>;
 }
